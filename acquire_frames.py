fe3ecdcbcaaa4a1a236546334eb15857bdc8fc91
Emre Kurtoglu <ekurtoglu@crimson.ua.edu>
2024-09-13 17:30:40 -0400

    eod

--- acquire_frames.py
+++ acquire_frames.py
@@ -4,6 +4,8 @@ import yaml
 import pprint
 import argparse
 import multiprocessing as mp
+from datetime import datetime
+from utils.basler import Basler
 from utils.arduino import Arduino
 from utils.helpers import str_to_bool
 
@@ -19,6 +21,10 @@ def main():
         help='Use this flag to save to disk. If not passed, will only view')
     parser.add_argument('-v', '--verbose', default=False, action='store_true',
         help='Use this flag to print debugging commands.')
+    parser.add_argument('-a', '--acquisition_mode', default='frames', # action='store_true',
+        help='Acquisition mode.')
+    parser.add_argument('--n_total_frames', default=3, type=int, # action='store_true',
+        help='Total number of frames to be acquired if --acquisiton_mode == frames.')
     parser.add_argument('--movie_format', default='opencv',
         choices=['hdf5','opencv', 'ffmpeg', 'directory'], type=str,
         help='Method to save files to movies. Dramatically affects performance and filesize')
@@ -29,14 +35,13 @@ def main():
          help='port for arduino (default: /dev/ttyACM0)')
     parser.add_argument('-r', '--acquisition_fps', default=30, type=float,
          help='Acquisition frame rate (default: 30 Hz)')
-    parser.add_argument('-w', '--videowrite_fps', default=None, type=float,
+    parser.add_argument('-w', '--videowrite_fps', default=30, type=float,
          help='Video save frame rate (default: acquisition rate)')
     parser.add_argument('-d', '--experiment_duration', default=float('inf'), type=float,
          help='Experiment dur in minutes (default: inf.)')
     parser.add_argument('-f', '--nframes_per_file', default=108000, type=int,
          help='N frames per file (default: 108000, or 15min at 120Hz)')
     parser.add_argument('-t', '--trigger_with_arduino', default="True",
-        #  action='store_false',
          type=str,
          help='Flag to use python software trigger (instead of arduino)')
     parser.add_argument('-N', '--nodemap_path', default=None,
@@ -58,7 +63,8 @@ def main():
         arduino = Arduino()
         arduino.initialize(port=args.port, baudrate=115200)
 
-    experiment = '%s_%s' % (time.strftime('%Y%m%d-%H%M%S', time.localtime()), args.name)
+    # experiment = '%s_%s' % (time.strftime('%Y-%m-%d_%H%M%S', time.localtime()), args.name)
+    experiment = datetime.now().strftime("%Y%m%d_%H_%M_%S_")  + args.name# microsec precision
 
     if str_to_bool(args.save):
         # update config to reflect runtime params
@@ -70,7 +76,7 @@ def main():
             with open(os.path.join(directory, 'loaded_config_file.yaml'), 'w') as f:
                 yaml.dump(config, f, default_flow_style=False, sort_keys=False)
     
-    start_t = time.perf_counter()
+    # start_t = time.perf_counter()
     # tuple_list=[]
     # for camname, cam in config['cams'].items():
     #     pprint.pprint(f'camname: {camname} \n cam: {cam}')
@@ -78,10 +84,22 @@ def main():
     #     #p = mp.Process(target=initialize_and_loop, args=(tup,))
     #     #p.start()
     #     tuple_list.append(tup)
+    # tuple_list = (config, camname, cam, args, experiment, start_t, trigger_with_arduino)
     camname = list(config['cams'].keys())[0]
     cam = config['cams'][camname]
-
-    tuple_list = (config, camname, cam, args, experiment, start_t, trigger_with_arduino)
+    pprint.pprint(f'camname: {camname} \n cam: {cam}')
+    cam1 = Basler(args, cam, cam_id=0)
+    
+    if args.acquisition_mode == 'frames' and not trigger_with_arduino:
+        frames = cam1.get_n_frames(args.n_total_frames)
+        # print(f'frames: {len(frames)}')
+        # frames.result()
+        
+    elif args.acquition_mode == 'duration':
+        start_time = time.time()
+        while time.time() < start_time:
+            frames = cam1.get_n_frames(1)
+        pass
 
 if __name__=='__main__':
     # set_start_method("spawn")
--- utils/basler.py
+++ utils/basler.py
@@ -1,52 +1,111 @@
+import os
+import cv2
+import time
+import json
+from datetime import datetime
 import pypylon
+import pprint
 from pypylon import pylon, genicam
+from concurrent.futures import ThreadPoolExecutor
+
+
+tp = ThreadPoolExecutor(10)  # max 10 threads
+
+def threaded(fn):
+    def wrapper(*args, **kwargs):
+        return tp.submit(fn, *args, **kwargs)  # returns Future object
+    return wrapper
 
 
 class Basler():
 
-    def __init__(self, max_cams=2, connect_retries=50) -> None:
+    def __init__(self, args, cam, cam_id=0, max_cams=2, connect_retries=20) -> None:
         print('Searching for camera...')
 
-        self.cameras = None
+        self.args = args
+        self.cam = cam
+        self.cam_id = cam_id
+        cameras = None
         # get transport layer factory
         self.tlFactory = pylon.TlFactory.GetInstance()
-
         # get the camera list 
         self.devices = self.tlFactory.EnumerateDevices()
-        print('Connecting to cameras...')   
+        
+        print('Connecting to the camera...')   
 
-        # Create array of cameras
         n = 0
-        while self.cameras is None and n < connect_retries:
+        while cameras is None and n < connect_retries:
             try:
-                self.cameras = pylon.InstantCameraArray(min(len(self.devices), max_cams))
-                l = self.cameras.GetSize()
-                #pylon.TlFactory.GetInstance().CreateFirstDevice())
-                print(f"Num. of cameras detected: {l}, cameras: {self.cameras}")
-                #time.sleep(0.5)
-                #camera.Open()
-                #print("Bound to device:" % (camera.GetDeviceInfo().GetModelName()))
+                cameras = pylon.InstantCameraArray(min(len(self.devices), max_cams))
+                self.camera = cameras[cam_id]
+                self.camera = pylon.InstantCamera(self.tlFactory.CreateDevice(self.devices[self.cam_id]))
+                # self.camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
+                # print(dir(self.camera))
+                # self.camera.MaxNumBuffer.Value = 20
+                print(f"Num. of cameras detected: {cameras.GetSize()}, selected cam_ID: {cam_id}")
+                self.init_camera()
 
             except Exception as e:
                 print('.')
-                # time.sleep(0.1)
-                # camera = None
+                time.sleep(0.1)
+                cameras = None
                 n += 1
 
-    def init_cameras(self):
-        for ix, cam in enumerate(self.cameras):
-            cam.Attach(self.tlFactory.CreateDevice(self.devices[ix]))
-            #camera.Open()
-            print("Bound to device: %s" % (cam.GetDeviceInfo().GetModelName()))
+    def init_camera(self):
+        
+        # self.camera.Attach(self.tlFactory.CreateDevice(self.devices[self.cam_id]))
+        
+        self.camera.Open()
+        # new_width = self.camera.Width.Value - self.camera.Width.Inc
+        # if new_width >= self.camera.Width.Min:
+        #     self.camera.Width.Value = new_width
+        self.camera.MaxNumBuffer.Value = 20
+        print(f"Cam {self.cam_id}, name: {self.camera.GetDeviceInfo().GetModelName()}, serial: {self.camera.DeviceInfo.GetSerialNumber()}")
+        # self.camera.SetCameraContext(self.cam_id)
+        print(f"Camera {self.cam_id} [{self.camera.GetDeviceInfo().GetModelName()}] successfully initialized!")
+    
+    @threaded
+    def get_n_frames(self, n_frames, timeout=5000,
+                     is_save_frames=False, is_save_metadata=False):
+        
+        # Start the grabbing of n_frames images.
+        # The camera device is parameterized with a default configuration which
+        # sets up free-running continuous acquisition.
+        self.camera.StartGrabbingMax(n_frames)
+        start_time = time.time()
+        # Camera.StopGrabbing() is called automatically by the RetrieveResult() method
+        # when n_frames images have been retrieved.
+        frames = []
+        metadata = {}
+        n = 0
+        while self.camera.IsGrabbing():
+            # Wait for an image and then retrieve it. A timeout of 5000 ms is used.
+            grabResult = self.camera.RetrieveResult(timeout, pylon.TimeoutHandling_ThrowException)
+            # Image grabbed successfully?
+            if grabResult.GrabSucceeded():
+                # Access the image data.
+                metadata['time_stamp'] = datetime.now().strftime("%Y%m%d_%H_%M_%S.%f")  # microsec precision
+                frames.append(grabResult.Array)
+                # print(f'Frame: {n}, time stamp: {metadata["time_stamp"]}')
+                # print("Gray value of first pixel: ", frames[-1][0, 0])
+                metadata['width'] = grabResult.Width
+                metadata['height'] = grabResult.Height
+                metadata['fps'] = self.camera.ResultingFrameRate.Value
+                metadata['frame_number'] = grabResult.ImageNumber
+                metadata['offset_x'] = grabResult.OffsetX
+                metadata['offset_y'] = grabResult.OffsetY
+                metadata['pylon_time_stamp'] = grabResult.TimeStamp
+                # pprint.pprint(metadata)
+            else:
+                print(f"Error: {grabResult.ErrorCode} --> {grabResult.ErrorDescription}")
+            grabResult.Release()
+            n += 1
+        print(f'Elapsed time for {n_frames} frames: {time.time() - start_time} sec.')
+        return frames
+    
+    def close(self):
+        self.camera.Close()
 
-        # open camera 
-        self.cameras.Open()
-        # store a unique number for each camera to identify the incoming images
-        for idx, cam in enumerate(self.cameras):
-            camera_serial = cam.DeviceInfo.GetSerialNumber()
-            print(f"set context {idx} for camera {camera_serial}")
-            cam.SetCameraContext(idx)
-        print("Cameras successfully initialized!")
     
 
     
