ac06750cbfe26240dea21cd542ea7faa525bed6b
Emre Kurtoglu <ekurtoglu@crimson.ua.edu>
2024-10-18 09:53:25 -0400

    eod

--- commit_git.sh
+++ commit_git.sh
@@ -1,3 +1,4 @@
+# conda env export | grep -v "^prefix: " > environment.yml
 # pip list --format=freeze > requirements.txt
 git add *.py *.txt *.sh */*.py */*.yaml *.md */*/*.ino */*.json */*.ipynb *.yml
 git commit -m "eod"
