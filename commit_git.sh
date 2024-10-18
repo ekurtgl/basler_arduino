efdee52e613a31f64ee5ceac6eea2d03e4c8f449
Emre Kurtoglu <ekurtoglu@crimson.ua.edu>
2024-10-18 09:51:33 -0400

    eod

--- commit_git.sh
+++ commit_git.sh
@@ -1,4 +1,4 @@
 # pip list --format=freeze > requirements.txt
-git add *.py *.txt *.sh */*.py */*.yaml *.md */*/*.ino */*.json */*.ipynb
+git add *.py *.txt *.sh */*.py */*.yaml *.md */*/*.ino */*.json */*.ipynb *.yml
 git commit -m "eod"
 git push -u origin main
